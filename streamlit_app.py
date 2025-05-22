a3119dd40e0b4dee3adbb9a40cfaf0559fe7e7ec
ardhitokhairan10 <ardhitokhairan10@gmail.com>
2025-05-22 11:10:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("DitoOoOo")
 st.write(
     "Ditoodimariii adaa dito jangan larii"
 )
-https://github.com/ardhitokhairan10/Dizzzz/blob/main/IMG-20250522-WA0001.jpeg
+IMG-20250522-WA0001.jpeg
